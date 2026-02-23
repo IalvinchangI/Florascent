a0aea3ba922dc6817cb80b3bef5208fc09542151
Alvin <99197625+IalvinchangI@users.noreply.github.com>
2026-02-23 23:45:49 +0800

    init

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABxgB0l9C8dGnGLoSw7KkOba59V2cMr48",
+  authDomain: "florascent2026.firebaseapp.com",
+  databaseURL: "https://florascent2026-default-rtdb.firebaseio.com",
+  projectId: "florascent2026",
+  storageBucket: "florascent2026.firebasestorage.app",
+  messagingSenderId: "1034431527621",
+  appId: "1:1034431527621:web:1c9155e4ef98c87cf6ceac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
