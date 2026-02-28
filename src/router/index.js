e229b05cbdf6b799d9500f6cff2067032cbb06b5
Alvin <99197625+IalvinchangI@users.noreply.github.com>
2026-02-28 21:27:24 +0800

    feat: update router
    - Add path in `router\index.js`
    - Add temporary AdminView
    - Add temporary ClientView

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ClientView from '../views/ClientView.vue'
+// import AdminView from '../views/AdminView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'client',
+      component: ClientView
+    },
+    // {
+    //   path: '/admin',
+    //   name: 'admin',
+    //   component: AdminView
+    // },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
+    }
+  ],
 })
 
 export default router
---
+++ src/views/ClientView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="client-container">
+    <LandscapeGuard v-if="role === ROLE.AUDIENCE" />
+
+    <Lobby v-if="role === ROLE.AUDIENCE && !userLang" @[LANG_SELECT]="setLang" />
+
+    <template v-else>
+      <!-- <Loading v-if="isLoading" /> -->
+      <dif v-if="isLoading" />
+
+      <template v-else>
+        <Waiting v-if="currentStage === Stage.Waiting" :role="role" :lang="userLang" />
+        <Intro v-else-if="currentStage === Stage.Intro" :role="role" :lang="userLang" :songData="songData" @[LANG_SELECT]="setLang" />
+        <!-- <Vote v-else-if="currentStage === 'Vote'" :role="role" :lang="userLang" :song="songData" /> -->
+        <!-- <Result v-else-if="currentStage === 'Result'" :role="role" :lang="userLang" :song="songData" /> -->
+        <!-- <Performance v-else-if="currentStage === 'Performance'" :role="role" :lang="userLang" /> -->
+        <!-- <Next v-else-if="currentStage === 'Next'" :role="role" :lang="userLang" /> -->
+      </template>
+    </template>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+// import { db } from '../firebase'; 
+// import { ref as dbRef, onValue } from 'firebase/database';
+import { LANG, ROLE, LANG_SELECT, Stage } from '@/constants.js';
+import {SongData, } from '@/song_data.js';
+
+import LandscapeGuard from '@/components/LandscapeGuard.vue';
+import Lobby from '@/components/Lobby.vue';
+// import Loading from '@/components/Loading.vue';
+import Waiting from '@/components/stage/Waiting.vue';
+import Intro from '@/components/stage/Intro.vue';
+// import Vote from '@/components/stage/Vote.vue';
+// import Result from '@/components/stage/Result.vue';
+// import Performance from '@/components/stage/Performance.vue';
+// import Next from '@/components/stage/Next.vue';
+
+const route = useRoute();
+// const role = ref(ROLE.PROJECTOR);
+const role = ref(ROLE.AUDIENCE);
+const isLoading = ref(false);  // ########################################### should be true
+
+// const userLang = ref(localStorage.getItem('slido_lang') || null);
+const userLang = ref(null);
+const setLang = (lang) => {
+  userLang.value = lang;
+  // localStorage.setItem('slido_lang', lang); // 存進瀏覽器，防重新整理
+};
+
+const currentStage = ref(Stage.Intro);
+// const currentStage = ref(Stage.Waiting);
+// const currentSongId = ref('song1');
+console.log(SongData);
+const songData = SongData;
+
+onMounted(() => {
+  if (route.query.role === ROLE.PROJECTOR) {
+    role.value = ROLE.PROJECTOR;
+  }
+});
+</script>
