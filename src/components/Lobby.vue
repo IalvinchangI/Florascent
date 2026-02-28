04b2460dba4c08d4c848361e66bcc2627d4e12f5
Alvin <99197625+IalvinchangI@users.noreply.github.com>
2026-02-28 21:27:48 +0800

    feat: add Lobby

---
+++ src/components/Lobby.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="lobby-page">
+    <div class="content-container">
+      
+      <div class="logo-wrapper">
+        <div class="img-box img-box-square img-restrict">
+          <img src="/src/assets/images/logo.png" alt="Logo"/>
+        </div>
+      </div>
+
+      <div class="text-section">
+        <p class="title-tw serif-font">請選擇您的語言</p>
+        <p class="title-en serif-font">Please select your language</p>
+      </div>
+
+      <div class="button-group">
+        <button class="base-btn default-btn animate-btn serif-font" @click="selectLanguage(LANG.TW)">繁體中文</button>
+        <button class="base-btn default-btn animate-btn serif-font" @click="selectLanguage(LANG.EN)">ENGLISH</button>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { LANG, LANG_SELECT } from '@/constants.js';
+
+const emit = defineEmits([LANG_SELECT]);
+
+const selectLanguage = (lang) => {
+  emit(LANG_SELECT, lang);
+};
+</script>
+
+<style scoped>
+.lobby-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 25%;
+  padding-bottom: 15%;
+  padding-left: 10%;
+  padding-right: 10%;
+}
+
+.content-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 500px;
+  gap: 2vh;
+}
+
+.logo-wrapper {
+  width: 70%;
+  padding-bottom: 15%;
+}
+
+.text-section {
+  text-align: center;
+}
+
+.button-group {
+  display: flex;
+  gap: 5vh;
+  width: 100%;
+}
+
+.button-group button {
+  flex: 1;
+}
+</style>
