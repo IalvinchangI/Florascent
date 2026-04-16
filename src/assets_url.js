1f7e1f0622fad8adcdf7c0b8f21d14a2a094b018
Alvin <99197625+IalvinchangI@users.noreply.github.com>
2026-04-16 22:48:40 +0800

    feat(assets): update their URL and preload them
    - Preload logo
    - Add template logo

--- src/assets_url.js
+++ src/assets_url.js
@@ -1,6 +1,6 @@
-
+// These assets are static and should be loaded before connecting to database
 export const BACKGROUND_HORIZONTAL_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/background_horizontal_ywrzpn.png";
 export const BACKGROUND_VERTICAL_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/background_vertical_ikhuov.png";
 
-export const LOGO_URL = "/assets/images/logo.png"
-export const QRCODE_URL = "/assets/images/qrcode.png"
+export const LOGO_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/character_kcffna.png"
+export const QRCODE_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/qrcode_q56pwn.png"
