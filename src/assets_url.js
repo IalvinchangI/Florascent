1a32be687afa80425bb2ab5501a60c9d94a62139
Alvin <99197625+IalvinchangI@users.noreply.github.com>
2026-04-17 16:30:36 +0800

    feat(asset): add `COUNTDOWN_SECOND_URLS `

--- src/assets_url.js
+++ src/assets_url.js
@@ -4,3 +4,36 @@ export const BACKGROUND_VERTICAL_URL = "https://res.cloudinary.com/dhbxvo9za/ima
 
 export const LOGO_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/character_kcffna.png"
 export const QRCODE_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/qrcode_q56pwn.png"
+
+export const COUNTDOWN_SECOND_URLS = [
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_1_ld0qgb.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_2_i6s64m.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_3_qy1gfe.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_4_jr43f8.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_5_jkbr5c.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_6_zvhhxi.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_7_xvyiqu.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_8_kqbufo.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_9_dkpbdg.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_10_vot3dh.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_11_y0xiii.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_12_qagmah.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_13_tcig2y.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_14_pdwpyf.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_15_hbajlm.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_16_eamald.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_17_mmtsly.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_18_aeluqo.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_19_vnztep.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_20_u11inv.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_21_k6bdlz.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_22_b6tyqc.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_23_qllsvg.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_24_v5he6a.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_25_kkfp4d.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_26_r571ns.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_27_sb0kz3.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_28_jjx0f4.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_29_brfqxp.png", 
+    "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/countdown_30_cahtqd.png"
+]
