e71a1913f0c2a87ba6f2fe0bce3d3ec42b28f8a5
Alvin <99197625+IalvinchangI@users.noreply.github.com>
2026-07-03 18:17:40 +0800

    chore: use github assets

--- src/assets_url.js
+++ src/assets_url.js
@@ -1,6 +1,8 @@
 // These assets are static and should be loaded before connecting to database
-export const BACKGROUND_HORIZONTAL_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/background_horizontal_ywrzpn.png";
-export const BACKGROUND_VERTICAL_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/background_vertical_ikhuov.png";
+// export const BACKGROUND_HORIZONTAL_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/background_horizontal_ywrzpn.png";
+// export const BACKGROUND_VERTICAL_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/background_vertical_ikhuov.png";
+export const BACKGROUND_HORIZONTAL_URL = "https://raw.githubusercontent.com/IalvinchangI/Florascent/master/assets/images/background_horizontal.png";
+export const BACKGROUND_VERTICAL_URL = "https://raw.githubusercontent.com/IalvinchangI/Florascent/master/assets/images/background_vertical.png";
 
 export const LOGO_URL = "https://res.cloudinary.com/dhbxvo9za/image/upload/f_auto/q_auto/v1776515569/logo_ehyzyc.png"
 export const LOGO_ICON_URL = "/logo.png"
